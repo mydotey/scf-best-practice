7af31b26a7f65e943bcb9c9e5726f9dbc381db8f
Qiang Zhao <koqizhao@outlook.com>
2018-08-18 23:11:02 +0800

    dotnet: add dotnet project structure

---
+++ dotnet/src/MyDotey.SCF.BP.LittleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDotey.SCF.BP.LittleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
